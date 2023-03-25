9b96fe33576310afcee57ef8eb8bfe6b9709da4c
Shizer <68175695+ImShizer@users.noreply.github.com>
2023-03-25 23:55:52 +0200

    Update firebaseConfig.js

--- API/firebaseConfig.js
+++ API/firebaseConfig.js
@@ -7,14 +7,14 @@ import { getReactNativePersistence } from "firebase/auth/react-native"
 import AsyncStorage from "@react-native-async-storage/async-storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVgRiFNlMQoGXZtGPTXeXTttutNU5pXmk",
-  authDomain: "evolunteer-71011.firebaseapp.com",
-  databaseURL: "https://evolunteer-71011-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "evolunteer-71011",
-  storageBucket: "evolunteer-71011.appspot.com",
-  messagingSenderId: "36502456450",
-  appId: "1:36502456450:web:594e75053515f3347ccc9b",
-  measurementId: "G-KT52BLBL7G"
+  apiKey: "---",
+  authDomain: "---",
+  databaseURL: "---",
+  projectId: "---",
+  storageBucket: "---",
+  messagingSenderId: "---",
+  appId: "---",
+  measurementId: "---"
 };
 
 
